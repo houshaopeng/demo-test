d3d5f276f188d418f36a99baf3b80c2d53468072
Sunny <Sunny>
2017-02-24 15:02:58 +0800

    版本1

---
+++ a.js
@@ -0,0 +1,12 @@
+/*
+* @Author: Marte
+* @Date:   2017-02-24 14:53:03
+* @Last Modified by:   Marte
+* @Last Modified time: 2017-02-24 15:01:45
+*/
+
+'use strict';
+test();
+function test(){
+    console.log("这是第一次写的代码")
+}
